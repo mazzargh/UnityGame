9a09331255a090c6a16505bc30aa86b5f2bb4fdf
Max <max@whisler.co.uk>
2019-09-21 17:47:48 +0100

    Revert "Added passthrough-platform functionality and rearranged level"
    This reverts commit 56dc9c02c5715cc76bb01f3d829aadfb204c0080.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -9,98 +9,60 @@ public class PlayerMovement : MonoBehaviour
     //public float acceleration;
 
     private bool facingRight = true;
-   // public LayerMask groundLayer;
-    public float jumpForce = 1;
+    public LayerMask groundLayer;
+    public float jumpForce = 400;
 
     public float gravity;
     public float stickToGroundForce;
     public float ySpeed;
-    private float hzSpeed;
-    public float wallSlideSpeed;
-    public float wallJumpForce = 1;
+
+    private bool justFell;
 
     private bool grounded = false;
     private bool jumping = true;
-    private bool wallSliding = false;
-
-    private bool centreMassGrounded = false;
-    private bool wallToRight = false;
-    private bool wallToLeft = false;
-    private bool hitCeiling = false;
 
     CharacterController controlTest;
-
-    private float inputDirection;
-    public Vector3 impactVector = Vector3.zero;
 
 
     // Start is called before the first frame update
     void Start()
     {
         //playerRB = GetComponent<Rigidbody>();
-
+        
         controlTest = GetComponent<CharacterController>();
     }
 
     private void Update()
     {
-        inputDirection = Input.GetAxis("Horizontal");
-        grounded = controlTest.isGrounded;
-        wallToLeft = CheckPlayerCollision(-Vector3.right, 1f, 1 << 10);
-        wallToRight = CheckPlayerCollision(Vector3.right, 1f, 1 << 10);
-        hitCeiling = CheckPlayerCollision(Vector3.up, 1.15f, 1 << 9 | 1 << 10);
-
-        if (hitCeiling)
-        {
-            Debug.Log("hitting");
-            ySpeed = -gravity/100;
-            impactVector.y = 0;
-        }
-        
-        if (!grounded && IsFacingWall() && inputDirection != 0 && ySpeed <= 0)
-        {
-            // wall slide
-            wallSliding = true;
-            ySpeed = -wallSlideSpeed*Time.deltaTime;
-        }
-        else if (wallSliding)
-        {
-            wallSliding = false;
-        }
-
-        if (Input.GetKeyDown("w"))
+        grounded = controlTest.isGrounded; //IsGrounded();
+        if (grounded && Input.GetKeyDown("w"))
         {   
-            if (grounded)
-            {
-                Jump();
-            }
-            else if (NextToWall())
-            {
-                WallJump();
-            }
+            Jump();
         }
+        //Debug.Log(grounded);
         CalculateGravity();
-        Rotate();
+        float inputDirection = Input.GetAxis("Horizontal");
+        Rotate(inputDirection);
         Move();
-
-        impactVector = Vector3.Lerp(impactVector, Vector3.zero, 5 * Time.deltaTime);
-
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-   
+        
+
+        
+        
     }
 
     private void Move()
     {
         //playerRB.velocity = new Vector3(inputDirection * maxSpeed, playerRB.velocity.y, 0);
 
-        Vector3 velocity = new Vector3(inputDirection * maxSpeed * Time.deltaTime, ySpeed + stickToGroundForce, 0);
+        Vector3 velocity = new Vector3(Input.GetAxis("Horizontal") * maxSpeed * Time.deltaTime, ySpeed + stickToGroundForce, 0);
         //transform.Translate(velocity);
 
-        controlTest.Move(velocity + impactVector);
+        controlTest.Move(velocity);
     }
 
     private void CalculateGravity()
@@ -108,15 +70,16 @@ public class PlayerMovement : MonoBehaviour
         if (grounded && ySpeed <= 0)
         {
             ySpeed = -gravity/100;
-            centreMassGrounded = CheckPlayerCollision(-Vector3.up, 1.2f, 1 << 9);
-            if (centreMassGrounded)
-            {
-                stickToGroundForce = -10;
-            }
-            else
+            if (!CentreMassIsGrounded())
             {
                 stickToGroundForce = 0;
             }
+            else
+            {
+                stickToGroundForce = -10;
+            }
+            
+
         }
         else
         {
@@ -125,70 +88,36 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    private void Rotate()
+    
+
+    private void Rotate(float inputDirection)
     {
         if ((inputDirection < 0 && facingRight) || (inputDirection > 0 && !facingRight))
         {
             facingRight = !facingRight;
             transform.Rotate(Vector3.up, 180);
-        }   
+        }
+        
     }
 
-    private bool IsFacingWall()
+    
+    private bool CentreMassIsGrounded()
     {
-        if ((facingRight && wallToRight) || (!facingRight && wallToLeft)) return true;
-        return false;
-    }
-
-    private bool NextToWall()
-    {
-        if (wallToLeft || wallToRight) return true;
-        return false;
-    }
-
-    private bool CheckPlayerCollision(Vector3 direction, float length, int layerMask)
-    {
+       
+        float length = 1.1f; // length of raycast to search for ground
         RaycastHit hit;
-        Debug.DrawRay(transform.position, direction * length, Color.green);
-        if (Physics.Raycast(transform.position, direction, out hit, length, layerMask))
+        Debug.DrawRay(transform.position, -Vector3.up, Color.red);
+        if (Physics.Raycast(transform.position, -Vector3.up, out hit, length, groundLayer))
         {
             return true;
         }
         return false;
     }
-
-    private bool HitCeiling()
-    {
-        return false;
-    }
     
+
     private void Jump()
     {
         ySpeed = jumpForce;
         //Debug.Log(ySpeed);
-    }
-
-    private void WallJump()
-    {
-        Vector3 dir = Vector3.zero;
-        dir.y = 1;
-        if (wallToRight)
-        {
-            Debug.Log("Wall to right");
-            // add impact top right
-            dir.x = -1; 
-        }
-        else
-        {
-            Debug.Log("Wall to left");
-            dir.x = 1;
-        }
-        AddImpact(dir);
-    }
-
-    private void AddImpact(Vector3 direction)
-    {
-        direction.Normalize();
-        impactVector += direction.normalized * wallJumpForce;
     }
 }
