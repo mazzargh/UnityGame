56dc9c02c5715cc76bb01f3d829aadfb204c0080
Max <max@whisler.co.uk>
2019-09-21 17:43:33 +0100

    Added passthrough-platform functionality and rearranged level
    Passthrough platforms have a problem with being on the ground layer currently

---
+++ Assets/Scripts/Environment/PassthruScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PassthruScript : MonoBehaviour
+{
+
+	public Collider collider1;
+	public Collider playerCollider;
+
+
+	private void OnTriggerEnter(Collider other) {
+		//Debug.Log("enter");
+		if (other.gameObject.tag == "Player") {
+			Physics.IgnoreCollision(collider1, other, true);
+		}
+	}
+
+	private void OnTriggerExit(Collider other) {
+		//Debug.Log("Exit");
+		if (other.gameObject.tag == "Player") {
+			Physics.IgnoreCollision(collider1, other, false);
+		}
+	}
+}
---
+++ Assets/Scripts/Environment/PressureTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressureTrigger : MonoBehaviour
+{
+	private void OnTriggerEnter(Collider other) {
+		//Debug.Log("enter");
+		if (other.gameObject.tag == "Player") {
+			
+		}
+	}
+
+	private void OnTriggerExit(Collider other) {
+		//Debug.Log("Exit");
+		if (other.gameObject.tag == "Player") {
+			
+		}
+	}
+}
